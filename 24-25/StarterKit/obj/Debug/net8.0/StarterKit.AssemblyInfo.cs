af85af7466bc3f11540b7c4e61a8bb777f7fe3de
marcelboo <101633688+marcelboo@users.noreply.github.com>
2024-09-25 16:37:20 +0200

    verwijderd wat overbodig was

--- 24-25/StarterKit/obj/Debug/net8.0/StarterKit.AssemblyInfo.cs
+++ 24-25/StarterKit/obj/Debug/net8.0/StarterKit.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("StarterKit")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+cd1cb35cd0409285763b7d7261d7fd2a6cc1f03d")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b92f3a71034e38b2ac7c1d6297c6148d8602e0fa")]
 [assembly: System.Reflection.AssemblyProductAttribute("StarterKit")]
 [assembly: System.Reflection.AssemblyTitleAttribute("StarterKit")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
