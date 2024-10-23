be9577bcbe7a93ed5ab0c987cb4195eac347e2c7
marcelboo <1038695@hr.nl>
2024-10-23 11:39:39 +0200

    customerservice af

--- 24-25/StarterKit/obj/Debug/net8.0/StarterKit.AssemblyInfo.cs
+++ 24-25/StarterKit/obj/Debug/net8.0/StarterKit.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("StarterKit")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+49df2fb910ef9280405a8d985ae1a28642c62143")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c0b72f6c1fd4f5fa6abac66f0d3af80792311e2a")]
 [assembly: System.Reflection.AssemblyProductAttribute("StarterKit")]
 [assembly: System.Reflection.AssemblyTitleAttribute("StarterKit")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
